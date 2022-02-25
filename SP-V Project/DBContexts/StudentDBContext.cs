f95303c84e3abd6454d0d4040637e62a351d80f6
shahjalal15 <98308344+shahjalal15@users.noreply.github.com>
2022-02-25 11:44:49 +0600

    Initial try regardnig our projrct

---
+++ SP-V Project/DBContexts/StudentDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SP_V_Project.Models;
+
+namespace SP_V_Project.DBContexts
+{
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext>options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ SP-V Project/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace SP_V_Project.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public int Age
+        {
+            get
+            {
+                string[] BirthDateSplitted = this.BirthDate.Split("-");
+                string[] CurrentDateSplitted = DateTime.Now.ToString("d-M-yyyy").Split("-");
+                int age = Int16.Parse(CurrentDateSplitted[2]) - Int16.Parse(BirthDateSplitted[2]);
+                if (Int16.Parse(CurrentDateSplitted[0]) < Int16.Parse(BirthDateSplitted[0]) &&
+                    Int16.Parse(CurrentDateSplitted[1]) < Int16.Parse(BirthDateSplitted[1]))
+                {
+                    age -= 1;
+                }
+                return age;
+            }
+            set {; }
+        }
+        public string Email { get; set; }
+        public string Section { get; set; }
+        public string Phone { get; set; }
+        public float CGPA { get; set; }
+        public string Gender { get; set; }
+    }
+}
+
+
